619a90dffc9935f7ff2a7c4f3babfa4e5e37aad9
appleEmily <emiri3128@gmail.com>
2019-05-06 00:55:13 +0900

    丸が動かせるようになりましたが、 ・一つしか出ない ・大きさが大きくなった(frame じゃなくなったから)

--- 隊形2019/StageViewController.swift
+++ 隊形2019/StageViewController.swift
@@ -21,6 +21,9 @@ class StageViewController: UIViewController {
     var speed: Double = 3.0
     var before_after: Bool = false
     
+    var gapX:CGFloat = 0.0
+    var gapY:CGFloat = 0.0
+    
     
     @IBOutlet weak var taikeiName: UILabel!
     
@@ -28,20 +31,11 @@ class StageViewController: UIViewController {
     @IBOutlet weak var beforeBtn: UIButton!
     @IBOutlet weak var afterBtn: UIButton!
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         taikeiName.text = text
-        
-        
     }
-    
-    
-    
-    
     @IBAction func goTop(_ sender: Any) {
         self.presentingViewController?.presentingViewController?.dismiss(animated: true, completion: nil)
     }
@@ -72,16 +66,19 @@ class StageViewController: UIViewController {
             
             humans[i].setNeedsDisplay()
         }
-        let newImage = UIImageView(frame: CGRect(x: -80, y: -80, width: 28, height: 28))
+        let newImage = UIImageView(image: UIImage(named: "people.png"))
+        
         newImage.tag = count
         newImage.image = UIImage(named: "humans.png")
-        newImage.center = self.view.center
+        newImage.center = CGPoint(x: view.center.x, y: view.center.y)
+        
+        newImage.isUserInteractionEnabled = true
+        
         humans.append(newImage)
         befores.append(self.view.center)
         afters.append(self.view.center)
-        self.view.addSubview(newImage)
+        view.addSubview(newImage)
         count += 1
-        
     }
     
     @IBAction func animation() {
@@ -102,20 +99,84 @@ class StageViewController: UIViewController {
             self.afterBtn.tintColor = UIColor.black
         }
     }
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touch = touches.first!
-        let view = touch.view!
-        let old = touch.previousLocation(in: self.view)
-        let new = touch.location(in: self.view)
-        view.frame.origin.x += (new.x - old.x)
-        view.frame.origin.y += (new.y - old.y)
-//currentHuman.center = location
-    }
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        let touch = touches.first! //このタッチイベントの場合確実に1つ以上タッチ点があるので`!`つけてOKです
+//        let location = touch.location(in: self.view) //in: には対象となるビューを入れます
+//        Swift.print(location)
+//
+//    }
+    
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let touch = touches.first! //このタッチイベントの場合確実に1つ以上タッチ点があるので`!`つけてOKです
-        let location = touch.location(in: self.view) //in: には対象となるビューを入れます
-        Swift.print(location)
+        // 最初にタッチした指のみ取得
+        if let touch = touches.first {
+            if let touchedView = touch.view {
+                
+                if count == 1 {
+                    gapX = touch.location(in: view).x - touchedView.center.x
+                    gapY = touch.location(in: view).y - touchedView.center.y
+                    touchedView.center = CGPoint(x: touch.location(in: view).x - gapX, y: touch.location(in: view).y - gapY)
+
+                }
+            }
+        }
     }
+    
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // touchesBeganと同じ処理だが、gapXとgapYはタッチ中で同じものを使い続ける
+        // 最初にタッチした指のみ取得
+        if let touch = touches.first {
+            // タッチしたビューをviewプロパティで取得する
+            if let touchedView = touch.view {
+                // tagでおじさんかそうでないかを判断する
+                if count == 1 {
+                    // gapX,gapYの取得は行わない
+                    touchedView.center = CGPoint(x: touch.location(in: view).x - gapX, y: touch.location(in: view).y - gapY)
+                }
+            }
+        }
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // gapXとgapYの初期化
+        gapX = 0.0
+        gapY = 0.0
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // touchesEndedと同じ処理
+        self.touchesEnded(touches, with: event)
+    }
+    
+    
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        // touchesBeganと同じ処理
+//        self.touchesBegan(touches, with: event)
+//    }
+    
+    
+    
+
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//
+//        let aTouch = touches.first //as! UITouch
+//        // 移動した先の座標を取得.
+//        let location = aTouch?.location(in: self.view)
+//        // 移動する前の座標を取得.
+//        let prevLocation = aTouch!.previousLocation(in: self.view)
+//        // CGRect生成.
+//        var myFrame: CGRect = self.view.frame
+//        // ドラッグで移動したx, y距離をとる.
+//        let deltaX: CGFloat = (location?.x)! - prevLocation.x
+//        let deltaY: CGFloat = location!.y - prevLocation.y
+//        // 移動した分の距離をmyFrameの座標にプラスする.
+//        myFrame.origin.x += deltaX
+//        myFrame.origin.y += deltaY
+//        // frameにmyFrameを追加.
+//        self.view.frame = myFrame
+//    }
+    
+    
     
 //    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
 //        let touchevent = touches.first!
@@ -125,7 +186,6 @@ class StageViewController: UIViewController {
 //        view.frame.origin.x += (new.x - old.x)
 //        view.frame.origin.y += (new.y - old.y)
 //    }
-    
     
     //きょめ
 //        override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -139,8 +199,7 @@ class StageViewController: UIViewController {
 //                afters[currentHuman.tag] = location
 //            }
 //        }
-    
-    
+
     //ここから下は昔エミリが書いたやつ未完成
     //おそらく　let の中身の定数にしたいものがはっきりしていない。
     //if let location = touch.location(in: self.view) {
@@ -150,6 +209,5 @@ class StageViewController: UIViewController {
     //} else {
     //     befores[currentHuman.tag] = location
     //    afters[currentHuman.tag] = location
-    
-    
+
 }

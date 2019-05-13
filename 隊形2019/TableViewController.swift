5a4724e8b3e0552f339daa40b2f29de15d32ee5d
appleEmily <emiri3128@gmail.com>
2019-05-14 01:22:31 +0900

    5/14

--- 隊形2019/TableViewController.swift
+++ 隊形2019/TableViewController.swift
@@ -13,22 +13,25 @@ import RealmSwift
 class TableViewController: UITableViewController {
     
     let realm = try! Realm()
-    var save = Save()
+    var data:Results<Save>!
+    
+    
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        data = realm.objects(Save.self)
+        //この行がわからない
+        //taikeiName.text = data[number].taikeiName
+        
+        
         
         tableView.delegate = self
         tableView.dataSource = self
         
         // tableViewにカスタムセルを登録
         tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -38,9 +41,6 @@ class TableViewController: UITableViewController {
         return cell
         
     }
-    
-
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0

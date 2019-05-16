710cea2773618cfcc2a9cf91101d4f111cdcaa36
appleEmily <emiri3128@gmail.com>
2019-05-16 16:31:26 +0900

    tableViewに出た

--- 隊形2019/TableViewController.swift
+++ 隊形2019/TableViewController.swift
@@ -16,37 +16,48 @@ class TableViewController: UITableViewController {
     //dataがとっていた変数を入れる箱
     var data:Results<Save>!
     
-    
-    
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+        
         data = realm.objects(Save.self)
-        //この行がわからない
-        //StageViewController にあるtaikeiName(String)をれrealmでほぞんしてある。その値を参照したい。
-        //taikeiName.text = data[number].taikeiName
         
-        
-        
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        // tableViewにカスタムセルを登録
-        tableView.register(UINib(nibName: "TableViewCell", bundle: nil), forCellReuseIdentifier: "TableViewCell")
+//        tableView.delegate = self
+//        tableView.dataSource = self
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+        
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return data.count
+          
+    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // セルの内容を取得
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as! TableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+        
+        let titleNameBox = data[indexPath.row].titleName
+        let bmoveX = data[indexPath.row].beforeMoveX
+        let bmoveY = data[indexPath.row].beforeMoveY
+        let amoveX = data[indexPath.row].afterMoveX
+        let amoveY = data[indexPath.row].afterMoveY
+        
+        cell.saveTitleName.text = titleNameBox
+        
         
         return cell
+    }
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
+        if editingStyle == UITableViewCell.EditingStyle.delete{
     }
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
     }
-
 }
